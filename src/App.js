61f96323c723df79bf9939a1b90289591da3dcb8
sirdashasan <sirdashasan@hotmail.com>
2024-02-27 22:34:11 +0300

    city temp and name completed

--- src/App.js
+++ src/App.js
@@ -1,23 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import axios from "axios";
+import "./App.css";
+import { useEffect } from "react";
+import { useState } from "react";
+import City from "./City";
 
 function App() {
+  const key = "5d4b1a1532473c491a6502d363adb68c";
+  const [city, setCity] = useState();
+  const [search, setSearch] = useState("");
+
+  useEffect(() => {
+    async function getApi() {
+      try {
+        const response = await axios.get(
+          `https://api.openweathermap.org/data/2.5/weather?q=${search}&appid=${key}&units=metric`
+        );
+        console.log(response);
+        setCity(response.data);
+      } catch (error) {
+        console.error(error);
+      }
+    }
+    getApi();
+  }, [search]);
+  console.log(search);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>
+        <input
+          onChange={(e) => setSearch(e.target.value)}
+          type="text"
+          placeholder="Placeholder"
+          className="px-2 w-[250px] py-1 placeholder-blueGray-300 text-blueGray-600 relative bg-white rounded text-sm border border-blueGray-300 outline-none focus:outline-none focus:ring w-full"
+        />
+        <City city={city} />
+      </div>
     </div>
   );
 }
---
+++ src/City.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const City = ({ city }) => {
+  console.log(city);
+
+  return (
+    <div>
+      <div>
+        <h1>{city.main.temp}</h1>
+        <h1>{city.name}</h1>
+      </div>
+    </div>
+  );
+};
+
+export default City;
